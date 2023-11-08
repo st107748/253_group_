1afeac2f63001cb47ccf9dab345285deb2eb1800
Лебединская Наталия Александровна <st007015@ad.pu.ru>
2023-11-08 10:25:01 +0300

    Заголовочный файл Matrix.h

---
+++ Matrix.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<vector>
+
+class Matrix
+{
+private:
+	int n; // число строк
+	int m; // число столбцов
+	vector <vector<double>> a;
+
+public:
+
+#include "Egor.h"
+#include "Aleksey.h"
+#include "Mariya.h"
+#include "Peidze.h"
+
+};
