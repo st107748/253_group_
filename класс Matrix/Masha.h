bb8974ae3421af571f0ee4a405f7ebeb5a318caa
nlebedin <149524669+nlebedin@users.noreply.github.com>
2023-11-15 17:45:53 +0300

    Update Masha.h

--- класс Matrix/Masha.h
+++ класс Matrix/Masha.h
@@ -1,4 +1,4 @@
-vector<double> Matrix::operator* (const vector<double>& v1);
-Matrix Matrix::operator| (const Matrix& b);
-double Matrix::MatrixNorm() const;
+vector<double> operator* (const vector<double>& v1);
+Matrix operator| (const Matrix& b);
+double MatrixNorm() const;
  
